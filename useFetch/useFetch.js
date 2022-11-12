af055f99a9883057529c12cc67623a6234041481
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-11 21:37:02 -0600

    feat: useFetch hook

---
+++ useFetch/useFetch.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null,
+    });
+
+    /**
+     * @description function to fetch data from the API
+     */
+    const getFetch = async () => {
+        setState({
+            isLoading: true,
+            ...state,
+        });
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setState({
+                data,
+                isLoading: false,
+                hasError: false,
+            });
+        } catch (error) {
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+            });
+        }
+    };
+
+    // every time the url changes, the useEffect will be called
+    useEffect(() => {
+        getFetch();
+    }, [url]);
+
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError,
+    };
+};
