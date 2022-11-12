c5e224e9cda65e71064fbbc7ad542717268cdb79
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-11-11 21:31:30 -0600

    feat: useCounter hook

---
+++ useCounter/useCounter.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const useCounter = (initalValue = 10) => {
+    
+    const [counter, setCounter] = useState(initalValue);
+    
+    const increment = (value = 1) => {
+        setCounter((current) => current + value);
+    }
+    
+    const decrement = (value = 1) => {
+        // if (counter === 1) return
+        setCounter((current) => current - value);
+    }
+    
+    const reset = () => {
+        setCounter(initalValue);
+    }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset
+    }
+}
